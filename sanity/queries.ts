32cc2c54cbc46ca69c1b224e6f91e9225e06ebec
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-21 19:37:51 +0200

    add groq query

---
+++ sanity/queries.ts
@@ -0,0 +1,12 @@
+import { groq } from 'next-sanity'
+
+export const postsQuery = groq`
+  *[_type == "post"] | order(publishedAt desc) {
+    _id,
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    "author": author->name,
+    "category": category->title,
+  }
